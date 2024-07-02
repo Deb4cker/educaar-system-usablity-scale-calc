68e11c070f22b58a8a8021929de8ff0fc92092a9
Nicolas <ndebacher@gmail.com>
2024-07-02 16:10:10 -0300

    deleting unused files

--- target/debug/deps/sus_point_calculator-91ffdb3582fe7316.d
+++
@@ -1,5 +0,0 @@
-/home/nicolas/Desktop/lab/rust/sus-point-calculator/target/debug/deps/libsus_point_calculator-91ffdb3582fe7316.rmeta: src/main.rs
-
-/home/nicolas/Desktop/lab/rust/sus-point-calculator/target/debug/deps/sus_point_calculator-91ffdb3582fe7316.d: src/main.rs
-
-src/main.rs:
--- target/debug/sus-point-calculator.d
+++
@@ -1,1 +0,0 @@
-/home/nicolas/Desktop/lab/rust/sus-point-calculator/target/debug/sus-point-calculator: /home/nicolas/Desktop/lab/rust/sus-point-calculator/src/main.rs
